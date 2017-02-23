2ee35e085644e0ee11054a67c7931175b368da67
Ant Cosentino <antony@mycosentino.com>
2017-02-23 10:26:36 +0200

    update main application

--- android/app/src/main/java/com/lifekeyrn/MainApplication.java
+++ android/app/src/main/java/com/lifekeyrn/MainApplication.java
@@ -1,21 +1,19 @@
 package com.lifekeyrn;
 
 import android.app.Application;
-import android.util.Log;
 
+import com.aakashns.reactnativedialogs.ReactNativeDialogsPackage;
 import com.facebook.react.ReactApplication;
-import com.lwansbrough.RCTCamera.RCTCameraPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.aakashns.reactnativedialogs.ReactNativeDialogsPackage;
-import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import za.co.io.reactnativecrypto.CryptoPackage;
+import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.lifekeyrn.pushnotification.PushNotificationPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -31,12 +29,13 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RCTCameraPackage(),
-            new VectorIconsPackage(),
-            new GoogleAnalyticsBridgePackage(),
-            new CryptoPackage(),
-            new ReactNativeDialogsPackage()
+              new MainReactPackage(),
+              new RCTCameraPackage(),
+              new VectorIconsPackage(),
+              new GoogleAnalyticsBridgePackage(),
+              new CryptoPackage(),
+              new PushNotificationPackage(),
+              new ReactNativeDialogsPackage()
       );
     }
   };
@@ -50,8 +49,5 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    MainApplication.this.mReactNativeHost.getReactInstanceManager().getCurrentReactContext()
-    .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-    .emit("nativeEvent", "something");
   }
 }
