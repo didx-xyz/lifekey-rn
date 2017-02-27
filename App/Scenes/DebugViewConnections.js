8dc0974d35e9c558e10707eb9cb7615befa8fd98
Ant Cosentino <antony@mycosentino.com>
2017-02-27 20:53:24 +0200

    list any active connections

--- App/Scenes/DebugViewConnections.js
+++ App/Scenes/DebugViewConnections.js
@@ -7,6 +7,7 @@
 
 import React from 'react'
 import Scene from '../Scene'
+import Session from '../Session'
 
 import {
   Text
@@ -15,22 +16,56 @@ import {
 import {
   Container,
   Content,
+  List,
+  ListItem
 } from 'native-base'
 import AndroidBackButton from 'react-native-android-back-button'
 
 export default class DebugViewConnections extends Scene {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      current_session: Session.getState()
+    }
+  }
 
   _hardwareBackHandler() {
     this.navigator.pop()
     return true
   }
 
+  _renderUserConnections(uc) {
+    return (
+      <ListItem>
+        <Text>{[
+          (this.state.current_session.users[uc.to_id] || {}).nickname,
+          (this.state.current_session.users[uc.from_id] || {}).nickname,
+        ].filter(n => !!n).pop()}</Text>
+      </ListItem>
+    )
+  }
+
   render() {
+    // get UCs from session storage
+    var uc_map = this.state.current_session.connections.user_connections || {}
+    // map it to an array of objects
+    var user_connections = Object.keys(uc_map).map(function(uc_key) {
+      return {
+        id: uc_map[uc_key].id,
+        from_id: uc_map[uc_key].from_id,
+        to_id: uc_map[uc_key].to_id
+      }
+    })
     return (
       <Container>
         <Content>
           <AndroidBackButton onPress={() => this._hardwareBackHandler()} />
-          <Text>Hello World</Text>
+          <ListItem itemHeader first>
+            <Text>CONNECTIONS</Text>
+          </ListItem>
+          <List dataArray={user_connections}
+                renderRow={this._renderUserConnections.bind(this)} />
         </Content>
       </Container>
     )
