1b3d5e95f220e06f3558f63d3733c6616372e697
Werner Roets <cobolt.exe@gmail.com>
2017-03-23 13:47:59 +0200

    implemented updateToken, completed getToken

--- App/Firebase.js
+++ App/Firebase.js
@@ -4,46 +4,90 @@
  * Civvals, 50 Seymour Street, London, England, W1H 7JG
  * @author Werner Roets <werner@io.co.za>
  */
+import { Platform } from 'react-native'
 import Session from './Session'
 import Storage from './Storage'
+import Crypto from './Crypto'
 import Config from './Config'
 import PushNotifications from './PushNotifications'
 import Logger from './Logger'
+import Api from './Api'
+import ConsentUser from './Models/ConsentUser'
+
 // A higher lever wrapper around PushNotifications
 export default class Firebase {
 
   static updateToken(token) {
+
+    const userState = ConsentUser.get()
+    let toSign
+
+    // Update persistent storage
     return Storage.store(Config.storage.dbKey, {
       firebaseToken: token
     })
+
+    // update volatile storage
     .then(() => {
       Logger.firebase(`Token updated: ${token}`)
-      Session.update({ firebaseToken: token })
-      Promise.resolve()
+      const update = {}[ConsentUser.storageKey] = { firebaseToken: token }
+      Session.update(update)
+      if (userState.password) {
+        return Crypto.loadKeyStore(Config.keystore.name, userState.password)
+      } else {
+        return Promise.reject('Not registered. Cannot notify server of token update.')
+      }
+    })
+
+    // Secure random
+    .then(name => Crypto.secureRandom())
+
+    // sign
+    .then(randomBytes => {
+      toSign = randomBytes
+      return Crypto.sign(
+        toSign,
+        Config.keystore.privateKeyName,
+        userState.password,
+        Crypto.SIG_SHA256_WITH_RSA
+      )
+    })
+
+    // Send to server
+    .then(signature => {
+      return Api.device({
+        user_id: userState.user_id,
+        plain: toSign,
+        signature: signature,
+        device_id: token,
+        device_platform: `${Config.APP_NAME} ${Platform.OS} v${Config.version}`
+      })
     })
   }
 
   static getToken() {
+
+    // First check Java env
     return PushNotifications.getToken()
+
     .then(tokenFromPN => {
       if (tokenFromPN) {
-        Session.update({ firebaseToken: tokenFromPN })
-        return Promise.resolve(tokenFromPN)
+        // Resolve if exists
+        Promise.resolve(tokenFromPN)
       } else {
-        const tokenFromSession = Session.getState().firebaseToken
-        if (tokenFromSession) {
-          return Promise.resolve(tokenFromSession)
+        // Check session
+        const userState = Session.getState().user
+        if (userState && userState.firebaseToken) {
+          Promise.resolve(userState.firebaseToken)
         } else {
+          // Storage is last chance
           return Storage.load(Config.storage.dbKey)
         }
       }
     })
-    .then(storage => {
-      if (storage.firebaseToken) {
-        return Promise.resolve(storage.firebaseToken)
-      } else {
-        return Promise.reject('No firebase token available')
-      }
+    .then(storageData => {
+      // The token in storage is reolved
+      Promise.resolve(storageData.user.firebaseToken)
     })
   }
 }
