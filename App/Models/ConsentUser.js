a7a11a12c6c836733026eebc275783fbff8216a1
Werner Roets <cobolt.exe@gmail.com>
2017-03-23 13:50:59 +0200

    completed and improved functions

--- App/Models/ConsentUser.js
+++ App/Models/ConsentUser.js
@@ -16,116 +16,121 @@ import ConsentKeystore from './ConsentKeystore'
 
 export default class ConsentUser {
 
-  static storageKey = 'users'
+  static storageKey = 'user'
 
   static get() {
-    return Session.getState().user
+    const userState = Session.getState()[ConsentUser.storageKey]
+    if (userState) {
+      return userState
+    } else {
+      // Initialise
+      const update = {}[ConsentUser.storageKey] = {}
+      Session.update(update)
+      // It's empty
+      return {}
+    }
   }
 
   static login(password) {
     // load keys maybe?
-    Session.update({ user: { loggedIn: true } })
+    const update = {}[ConsentUser.storageKey] = {
+      password: password,
+      loggedIn: true
+    }
+    Session.update(update)
   }
 
   static logout() {
-    Session.update({ user: { loggedIn: false } })
+    // YOU CAN NEVER LEAVE CONSENT
   }
 
   static setDid(did) {
-    return Storage.store(Config.storage.dbKey, {
-      user: { dbDid: did }
-    })
+    const update = {}[ConsentUser.storageKey] = { dbDid: did }
+    return Storage.store(Config.storage.dbKey, update)
     .then(() => {
-      Session.update({ user: { dbDid: did } })
-      Promise.resolve(did)
+      Session.update(update)
+      return Promise.resolve(did)
     })
   }
 
   static register(username, email, password) {
-    return new Promise((resolve, reject) => {
-      let publicKeyPem, firebaseToken
-      const toSign = Date.now().toString() // should be a random
 
-      // Check if already registered
-      return ConsentUser.isRegistered()
-      .then(registered => {
-        if (registered) {
-          Promise.reject('Already registered')
-        } else {
-          // create keystore first
-          return Crypto.createKeyStore(Config.keystore.name, password)
-          // Create the user's keypair
+    let publicKeyPem, firebaseToken, toSign
 
-        }
-      })
+    // Check if already registered
+    return ConsentUser.isRegistered()
+    .then(registered => {
+      if (registered) {
+        return Promise.reject('Already registered')
+      } else {
+        // create keystore first
+        return Crypto.createKeyStore(Config.keystore.name, password)
+      }
+    })
 
-      .then(() => Crypto.addKeyPair(
-        Crypto.KEYPAIR_RSA,
-        Config.keystore.name,
-        2048,
+    // Create the user's keypair
+    .then(() => Crypto.addKeyPair(
+      Crypto.KEYPAIR_RSA,
+      Config.keystore.name,
+      2048,
+      password,
+      Config.keystore.pemCertificatePath
+    ))
+
+    // Get public key in PEM format
+    .then(_keys => Crypto.getKeyAsPem(Config.keystore.publicKeyName, password))
+
+    // Get firebase token
+    .then(_publicKeyPem => {
+      publicKeyPem = _publicKeyPem
+      return Firebase.getToken()
+    })
+    .then(_firebaseToken => {
+      firebaseToken = _firebaseToken
+      return Crypto.secureRandom()
+    })
+    // Sign with private key
+    .then(_secureRandom => {
+      toSign = _secureRandom
+      return Crypto.sign(
+        toSign,
+        Config.keystore.privateKeyName,
         password,
-        Config.keystore.pemCertificatePath
-      ))
-      // Get public key in PEM format
-      .then(_keys => {
-        console.log(JSON.stringify(_keys))
-        Crypto.getKeyAsPem(`public${Config.keystore.keyName}`, password)
-      })
+        Crypto.SIG_SHA256_WITH_RSA
+      )
+    })
 
-      // Get firebase token
-      .then(_publicKeyPem => {
-        publicKeyPem = _publicKeyPem
-        return Firebase.getToken()
-      })
+    // Send to server
+    .then(signature => Api.register({
+      email: email,
+      nickname: username,
+      device_id: firebaseToken,
+      device_platform: Platform.OS,
+      public_key_algorithm: Config.keystore.publicKeyAlgorithm,
+      public_key: publicKeyPem,
+      plaintext_proof: toSign,
+      signed_proof: signature
+    }))
 
-      // Sign with private key
-      .then(_firebaseToken => {
-        firebaseToken = _firebaseToken
-        return Crypto.sign(
-          toSign,
-          `private_${Config.keystore.keyName}`,
-          password,
-          Crypto.SIG_SHA256_WITH_RSA
-        )
-      })
-
-      // Send to server
-      .then(signature => Api.register({
-        email: email,
-        nickname: username,
-        device_id: firebaseToken,
-        device_platform: Platform.OS,
-        public_key_algorithm: Config.keystore.publicKeyAlgorithm,
-        public_key: publicKeyPem,
-        plaintext_proof: toSign,
-        signed_proof: signature
-      }))
-
-      // Update session and storage
-      .then(response => {
-        const jsonData = response.body
-        Session.update({
-          user: {
-            dbId: jsonData.id,
-            password: password,
-            email: email
-          }
-        })
-        return Storage.store(Config.storage.dbKey, {
-          user: {
-            dbId: jsonData.id,
-            email: email
-          }
-        })
-      })
-      .then(() => {
-        Logger.info('User registered')
-        Promise.resolve()
-      })
-      .catch(error => {
-        Logger.error('Could not register user', 'ConsentUser', error)
-        reject(error)
-      })
+    // Get response from server
+    .then(response => {
+      const jsonData = response.body
+      // Store in session and persistent storage
+      const sessionUpdate = {}[ConsentUser.storageKey] = {
+        dbId: jsonData.id,
+        password: password,
+        email: email
+      }
+      const storageUpdate = {}[ConsentUser.storageKey] = {
+        dbId: jsonData.id,
+        email: email
+      }
+      Session.update(sessionUpdate)
+      return Storage.store(Config.storage.dbKey, storageUpdate)
+    })
+    .then(() => {
+      Logger.info('User registered')
+      return Promise.resolve()
     })
   }
 
@@ -134,19 +139,17 @@ export default class ConsentUser {
   }
 
   static isRegistered() {
-    return new Promise((resolve, reject) => {
-      return ConsentKeystore.exists()
-      .then(exists => {
-        if (exists) {
-          Logger.info(`Keystore "${Config.keystore.name}" found. User not registered.`, this.filename)
-          Session.update({ user: { registered: true } })
-          resolve(true)
-        } else {
-          Logger.info(`Keystore "${Config.keystore.name}" not found. User not registered.`, this.filename)
-          Session.update({ user: { registered: false } })
-          resolve(false)
-        }
-      })
+    return ConsentKeystore.exists()
+    .then(exists => {
+      if (exists) {
+        Logger.info(`Keystore "${Config.keystore.name}" found. User not registered.`, this.filename)
+        Session.update({ user: { registered: true } })
+        return Promise.resolve(true)
+      } else {
+        Logger.info(`Keystore "${Config.keystore.name}" not found. User not registered.`, this.filename)
+        Session.update({ user: { registered: false } })
+        return Promise.resolve(false)
+      }
     })
   }
 }
