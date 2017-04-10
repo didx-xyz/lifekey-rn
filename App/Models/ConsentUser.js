2a8cb34abc9ac2bbe0f72e5b3ca809594c743178
Werner Roets <cobolt.exe@gmail.com>
2017-04-10 22:00:28 +0200

    better error handling, better login, better logout

--- App/Models/ConsentUser.js
+++ App/Models/ConsentUser.js
@@ -4,6 +4,7 @@
  * Civvals, 50 Seymour Street, London, England, W1H 7JG
  * @author Werner Roets <werner@io.co.za>
  */
+
 import { Platform, AsyncStorage } from 'react-native'
 import Session from '../Session'
 import Crypto from '../Crypto'
@@ -13,60 +14,161 @@ import Api from '../Api'
 import ConsentConnection from './ConsentConnection'
 import ConsentConnectionRequest from './ConsentConnectionRequest'
 import ConsentDiscoveredUser from './ConsentDiscoveredUser'
+import ConsentError from '../ConsentError'
+
+export const E_MUST_BE_REGISTERED = 0x01
+export const E_INCORRECT_PASSWORD_FOR_KEYSTORE = 0x02
+export const E_DOES_NOT_EXIST_ON_SERVER = 0x03
+export const E_COULD_NOT_LOGOUT = 0x04
+export const E_USER_DOES_NOT_EXIST = 0x05
+export const E_NO_FIREBASE_TOKEN = 0x06
+export const E_COULD_NOT_SET_ITEM = 0x07
+export const E_SERVER_ERROR = 0x08
+export const E_MUST_BE_LOGGED_IN = 0x09
+
+const STORAGE_KEY = 'user'
+const LOGTAG = 'ConsentUser'
 
 export default class ConsentUser {
 
   static storageKey = 'user'
   static filename = 'ConsentUser.js'
 
+  /**
+   * Get the device user
+   * @param {number} id The id of the user
+   * @returns {object} user The user
+   * @throws {Error} E_USER_DOES_NOT_EXIST
+   */
   static get() {
-    return AsyncStorage.getItem(ConsentUser.storageKey)
+    return AsyncStorage.getItem(STORAGE_KEY)
     .then(itemJSON => {
       try {
         const user = JSON.parse(itemJSON)
         if (user) {
           return Promise.resolve(user)
         }
-      } catch(error) {
+      } catch (error) {
         // json parse error
-        return Promise.reject(error)
+        return Promise.reject(
+          new ConsentError(error, E_USER_DOES_NOT_EXIST)
+        )
       }
     })
   }
 
+  /**
+   * Log the user in
+   * @param {string} password The password or pin of the keystore
+   * @returns {object} user The user's details
+   * @throws E_MUST_BE_REGISTERED
+   *         E_INCORRECT_PASSWORD_FOR_KEYSTORE
+   *         E_DOES_NOT_EXIST_ON_SERVER
+   */
   static login(password) {
-    // load keys
-    // set password in state
-    // set loggedIn true in state
-    return Crypto.loadKeyStore(Config.keystore.name, password)
-    .then(loadedKeystore => {
-      Logger.info('Keystore loaded, user verified', this.filename)
-      const update = {}
-      update[ConsentUser.storageKey] = {
-        password: password,
-        loggedIn: true
+    let user
+
+    // Get from model
+    return ConsentUser.get()
+    .then(_user => {
+
+      user = _user
+      if (user.registered) {
+
+        // Try to unlock keystore
+        return Crypto.loadKeyStore(Config.keystore.name, password)
+
+      } else {
+
+        // Trying to loging without being registered
+        return Promise.reject(
+          new ConsentError(
+            'Cannot log in of not registered',
+            E_MUST_BE_REGISTERED
+          )
+        )
+
       }
-      Session.update(update)
+    })
+    .then(loadedKeystore => {
+      if (loadedKeystore) {
+
+        // Unlocked keystore with password
+        Logger.info('Keystore loaded, password verified', LOGTAG)
+
+        // Check if user exists on the other side
+        return Api.profile({ id: user.id })
+
+      } else {
+
+        // Wrong password
+        return Promise.reject(
+          new ConsentError(
+            'Incorrect password for keystore',
+            E_INCORRECT_PASSWORD_FOR_KEYSTORE
+          )
+        )
+
+      }
+    })
+    .then(response => {
+      if (parseInt(response.status) === 200) {
+
+        // profile exists too, seems legit, log them in
+        const update = {}
+        update[STORAGE_KEY] = {
+          password: password,
+          loggedIn: true
+        }
+        Session.update(update)
+
+        return Promise.resolve(user)
+      } else {
+        // No such user on server
+        Promise.reject(
+          new ConsentError('User does not exist on server', E_DOES_NOT_EXIST_ON_SERVER)
+        )
+      }
+    })
+  }
+
+  /**
+   * Log the user out
+   * Unload the keystore, requiring a password to use it again
+   * also remove the password from session and set loggedIn false
+   * @returns {undefined}
+   * @throws E_COULD_NOT_LOGOUT
+   */
+  static logout() {
+    return Crypto.unloadKeyStore()
+    .then(() => {
+      Session.update({ user: { loggedIn: false, password: '' } })
       return Promise.resolve()
     })
     .catch(error => {
-      return Promise.reject(error)
+      return Promise.reject(
+        new ConsentError(error, E_COULD_NOT_LOGOUT)
+      )
     })
   }
 
-  static logout() {
-    // YOU CAN NEVER LEAVE CONSENT
-    // Clear password from session
-    // maybe unload keystore?
-  }
-
+  /**
+   * Get the user's Firebase token
+   * @returns {string} firebaseToken The firebase token
+   * TODO: Check RAM first then fallback to sqlite
+   */
   static getToken() {
-    return AsyncStorage.getItem(ConsentUser.storageKey)
+    return AsyncStorage.getItem(STORAGE_KEY)
     .then(userJSON => {
       if (userJSON) {
         const user = JSON.parse(userJSON)
         if (!user || !user.firebaseToken) {
-          return Promise.resolve(null)
+          return Promise.resolve(
+            new ConsentError(
+              'No firebase token available',
+              E_NO_FIREBASE_TOKEN
+            )
+          )
         } else {
           return Promise.resolve(user.firebaseToken)
         }
@@ -76,24 +178,37 @@ export default class ConsentUser {
     })
   }
 
+  /**
+   * Set the user's Firebase token
+   * @param {string} firebaseToken The firebase token
+   */
   static setToken(firebaseToken) {
     let toSign
-    return AsyncStorage.getItem(ConsentUser.storageKey)
+    return AsyncStorage.getItem(STORAGE_KEY)
     .then(userJSON => {
       // It existed before
       if (userJSON) {
         const user = JSON.parse(userJSON)
 
         const updatedUser = Object.assign(user, { firebaseToken })
-        return AsyncStorage.setItem(ConsentUser.storageKey, JSON.stringify(updatedUser))
+        return AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(updatedUser))
       } else {
         // set it for the first time
-        return AsyncStorage.setItem(ConsentUser.storageKey, JSON.stringify({ firebaseToken: firebaseToken }))
+        return AsyncStorage.setItem(
+          STORAGE_KEY,
+          JSON.stringify({ firebaseToken: firebaseToken })
+        )
       }
     })
     .then(error => {
-      if(error) {
-        return Promise.reject(error)
+      if (error) {
+        // return Promise.reject(error)
+        return Promise.reject(
+          new ConsentError(
+            'Could not save record',
+            E_COULD_NOT_SET_ITEM
+          )
+        )
       } else {
         Logger.firebase(`Token updated ${firebaseToken}`)
         // We gotta notify the server if we can
@@ -104,13 +219,18 @@ export default class ConsentUser {
     .then(registered => {
       if (registered) {
         Logger.firebase('Notifying server of token update')
-        return Crypto.loadKeyStore(Config.keystore.name, ConsentUser.getPassword())
+        return Crypto.loadKeyStore(Config.keystore.name, ConsentUser.getPasswordSync())
       } else {
-        return Promise.reject('Must be registered to notify server of token update')
+        return Promise.rejecet(
+          new ConsentError(
+            'Must be registered to notify server of token update',
+            E_MUST_BE_REGISTERED
+          )
+        )
       }
     })
     // Get secure random
-    .then(name => {
+    .then(_keystoreName => {
       Logger.firebase('Getting secure random...')
       return Crypto.secureRandom()
     })
@@ -121,7 +241,7 @@ export default class ConsentUser {
       return Crypto.sign(
         toSign,
         Config.keystore.privateKeyName,
-        ConsentUser.getPassword(),
+        ConsentUser.getPasswordSync(),
         Crypto.SIG_SHA256_WITH_RSA
       )
     })
@@ -129,7 +249,7 @@ export default class ConsentUser {
     .then(signature => {
       Logger.firebase('Sending to server...')
       return Api.device({
-        user_id: ConsentUser.getId(),
+        user_id: ConsentUser.getIdSync(),
         plain: toSign,
         signature: signature,
         device_id: firebaseToken,
@@ -138,53 +258,75 @@ export default class ConsentUser {
     })
     .then(response => {
       if (response.error) {
-        return Promise.reject('Server responded with error ' + JSON.stringify(response.error))
+        return Promise.reject(
+          new ConsentError('Server responded with error',E_SERVER_ERROR)
+        )
       } else {
         return Promise.resolve('Server notified of token update')
       }
     })
   }
 
-  static getPassword() {
+  /**
+   * Get the user's password synchronously
+   * @returns {string} password The user's password
+   */
+  static getPasswordSync() {
     const state = Session.getState()
     if (!state || !state.user || !state.user.password) {
-      throw 'No password set, user is not logged in'
+      throw new ConsentError(
+        'No password set, user is not logged in',
+        E_MUST_BE_LOGGED_IN
+      )
     } else {
       return state.user.password
     }
   }
 
-  /*
-   * We need this function because DIDs are received through their
-   * own channel and may only be received at a later time after registration
+  /**
+   * Set the DID of the user
+   * @param {string} did The did to assigned to the user
+   * @returns {string} did The did assigned to the user
    */
   static setDid(did) {
-    return AsyncStorage.getItem(ConsentUser.storageKey)
+    return AsyncStorage.getItem(STORAGE_KEY)
     .then(itemJSON => {
       if (itemJSON) {
         try {
+          // Update session
+          Session.update(Object.assign(Session.getState().user, { did }))
           const user = JSON.parse(itemJSON)
           const userUpdate = Object.assign(user, { did })
-          return AsyncStorage.setItem(ConsentUser.storageKey, JSON.stringify(userUpdate))
+          return AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(userUpdate))
         } catch (error) {
-          return Promise.reject(error)
+          return Promise.reject(
+            new ConsentError(
+              `Could not set did for ${STORAGE_KEY}`,
+              E_COULD_NOT_SET_ITEM
+            )
+          )
         }
 
       } else {
         // create first record
-        return AsyncStorage.setItem(ConsentUser.storageKey, JSON.stringify({ did }))
+        return AsyncStorage.setItem(STORAGE_KEY, JSON.stringify({ did }))
       }
     })
     .then(error => {
       if (error) {
-        return Promise.reject(error)
+        return Promise.reject(
+          new ConsentError(
+            'Could not set item',
+            E_COULD_NOT_SET_ITEM
+          )
+        )
       } else {
         return Promise.resolve(did)
       }
     })
   }
 
-  static getId() {
+  static getIdSync() {
     const state = Session.getState()
     if (!state || !state.user || !state.user.id) {
       return null
@@ -195,7 +337,7 @@ export default class ConsentUser {
 
   static unregister() {
     let firebaseToken
-    return AsyncStorage.getItem(ConsentUser.storageKey)
+    return AsyncStorage.getItem(STORAGE_KEY)
     .then(itemJSON => {
       const user = JSON.parse(itemJSON)
       if (user) {
@@ -216,7 +358,7 @@ export default class ConsentUser {
       return Promise.all([
         Crypto.deleteKeyStore(Config.keystore.name),
         AsyncStorage.multiRemove([
-          ConsentUser.storageKey,
+          STORAGE_KEY,
           ConsentConnection.storageKey,
           ConsentConnectionRequest.storageKey,
           ConsentDiscoveredUser.storageKey
@@ -226,13 +368,13 @@ export default class ConsentUser {
     .then(results => {
       if (results[1]) {
         if (results[1].error) {
-          Logger.error('Could not purge databases', this.filename, results[1].error)
+          Logger.error('Could not purge databases', LOGTAG, results[1].error)
+          return Promise.reject('Could not purge databases')
         }
       } else {
-        return AsyncStorage.setItem(ConsentUser.storageKey, {
+        return AsyncStorage.setItem(STORAGE_KEY, JSON.stringify({
           user: { firebaseToken: firebaseToken }
-        })
-        // return Promise.resolve()
+        }))
       }
     })
   }
@@ -240,16 +382,16 @@ export default class ConsentUser {
   static register(username, email, password) {
 
     let publicKeyPem, firebaseToken, toSign, userID
-    Logger.info(`Registering as: ${username}, ${email}, ${password}`, this.filename)
+    Logger.info(`Registering as: ${username}, ${email}, ${password}`, LOGTAG)
 
     return ConsentUser.getToken()
     .then(_firebaseToken => {
       if (_firebaseToken) {
-        Logger.info('Fetching secure random', this.filename)
+        Logger.info('Fetching secure random', LOGTAG)
         firebaseToken = _firebaseToken
         return Crypto.getKeyStoreList()
       } else {
-        Logger.firebase('No firebase token available', this.filename)
+        Logger.firebase('No firebase token available', LOGTAG)
         return Promise.reject('No token available. Cannot register')
       }
     })
@@ -265,14 +407,14 @@ export default class ConsentUser {
 
         })
       } else {
-        Logger.info('Creating new keystore', this.filename)
+        Logger.info('Creating new keystore', LOGTAG)
         return Crypto.createKeyStore(Config.keystore.name, password)
       }
     })
 
     // Create the user's keypair
     .then(() => {
-      Logger.info('Creating keypair', this.filename)
+      Logger.info('Creating keypair', LOGTAG)
       return Crypto.addKeyPair(
         Crypto.KEYPAIR_RSA,
         Config.keystore.keyName,
@@ -284,20 +426,20 @@ export default class ConsentUser {
 
     // Get public key in PEM format
     .then(_keys => {
-      Logger.info('Converting public key to PEM format', this.filename)
+      Logger.info('Converting public key to PEM format', LOGTAG)
       return Crypto.getKeyAsPem(Config.keystore.publicKeyName, password)
     })
 
     // Get firebase token
     .then(_publicKeyPem => {
       publicKeyPem = _publicKeyPem
-      Logger.info('Fetching secure random', this.filename)
+      Logger.info('Fetching secure random', LOGTAG)
       return Crypto.secureRandom()
     })
 
     // Sign with private key
     .then(_secureRandom => {
-      Logger.info('Signing...', this.filename)
+      Logger.info('Signing...', LOGTAG)
       toSign = _secureRandom
       return Crypto.sign(
         toSign,
@@ -309,7 +451,7 @@ export default class ConsentUser {
 
     // Send to server
     .then(signature => {
-      Logger.info('Sending details to server', this.filename)
+      Logger.info('Sending details to server', LOGTAG)
       return Api.register({
         email: email.trim(),
         nickname: username.trim(),
@@ -327,8 +469,8 @@ export default class ConsentUser {
       if (response.error === true) {
         // Server rejected registration
         // Remove keystore
-        Logger.info('Registration on server failed', this.filename)
-        Logger.info('Deleting keystore', this.filename)
+        Logger.info('Registration on server failed', LOGTAG)
+        Logger.info('Deleting keystore', LOGTAG)
         return Crypto.deleteKeyStore(Config.keystore.name)
         .then(() => {
           return Promise.reject(response.error)
@@ -337,15 +479,16 @@ export default class ConsentUser {
       const jsonData = response.body
       // TODO: check server response
       // user could already exist etc
-      Logger.info('Server responded', this.filename)
+      Logger.info('Server responded', LOGTAG)
       userID = jsonData.id
 
       // See what's currently in the DB
-      return AsyncStorage.setItem(ConsentUser.storageKey, JSON.stringify({
+      return AsyncStorage.setItem(STORAGE_KEY, JSON.stringify({
         id: userID,
+        did: null,
         email: email,
         firebaseToken: firebaseToken,
-        registered: true
+        registered: true  // We will get this later
       }))
     })
 
@@ -353,13 +496,14 @@ export default class ConsentUser {
     .then(error => {
       if (error) {
         // If we couldn't write to database
-        Logger.error('Could not write to AsyncStorage', this.filename, error)
+        Logger.error('Could not write to AsyncStorage', LOGTAG, error)
         return Promise.reject(error)
       } else {
         // Update state
         const sessionUpdate = {
           user: {
             id: userID,
+            did: null,
             password: password,
             email: email,
             registered: true,
@@ -367,7 +511,7 @@ export default class ConsentUser {
           }
         }
         Session.update(sessionUpdate)
-        Logger.info('User registered', this.filename)
+        Logger.info('User registered', LOGTAG)
         return Promise.resolve({
           id: userID
         })
@@ -389,7 +533,7 @@ export default class ConsentUser {
   }
 
   static isRegistered() {
-    return AsyncStorage.getItem(ConsentUser.storageKey)
+    return AsyncStorage.getItem(STORAGE_KEY)
     .then(itemJSON => {
       if (!itemJSON) {
         return Promise.resolve(false)
