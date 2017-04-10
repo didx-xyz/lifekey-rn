5cf2cc0c9b78cf15ad11719051c0c11b80399760
Werner Roets <cobolt.exe@gmail.com>
2017-04-10 21:55:23 +0200

    added error codes, fixed mistake

--- App/ConsentError.js
+++ App/ConsentError.js
@@ -10,17 +10,20 @@ export const ErrorCode = {
   E_UNKNOWN: 0x029a,
   // 1000 RECOVERABLE
   E_API_ERROR: 0x1011,
-  E_ACCESS_KEYSTORE_NOT_LOADED: 0x0021
+  E_ACCESS_KEYSTORE_NOT_LOADED: 0x1021,
+  E_CONSENT_USER_COULD_NOT_LOGIN: 0x1031,
+  E_CONSENT_USER_COULD_NOT_LOGOUT: 0x1032,
   // 6000 FATAL
+  E_API_FATAL_ERROR: 0x6011
 }
 
 export default class ConsentError extends Error {
 
   constructor(message, code) {
     super(message)
-    this.name = 'ConsentError'
+    this.name = message || 'ConsentError'
     this.message = message || 'ConsentError'
-    this.code = 0x29a || code
+    this.code = code || 0x29a
     Logger.error(message, this.code)
   }
 
