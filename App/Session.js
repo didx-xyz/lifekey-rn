bef00fdd1a6a3488c3fd7f01e3eb3de9125b5901
Werner Roets <cobolt.exe@gmail.com>
2017-04-13 14:37:21 +0200

    made state private

--- App/Session.js
+++ App/Session.js
@@ -8,18 +8,18 @@
 import Logger from './Logger'
 import deepmerge from 'deepmerge'
 
+var state = {}
 /**
  * A static class to store and retrieve a global state
  */
 export default class Session {
 
-  static state = {}
   /**
    * Return an Object representing the current state
    * @returns {Object} data The current state held in the store
    */
   static getState() {
-    return this.state
+    return state
   }
 
   /**
@@ -30,8 +30,8 @@ export default class Session {
    */
   static update(data) {
     if (typeof data === 'object') {
-      this.state = deepmerge(this.state, data)
-      Logger.session(JSON.stringify(this.state))
+      state = deepmerge(state, data)
+      Logger.session(JSON.stringify(state))
     } else {
       throw 'Update only accepts objects'
     }
