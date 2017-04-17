13a4aa5f2d4ad1b537b3eca38c50ca0739c6dbd8
Werner Roets <cobolt.exe@gmail.com>
2017-04-17 16:01:08 +0200

    added checkParameters to Util

--- App/Util.js
+++ App/Util.js
@@ -4,6 +4,53 @@ class Util {
     return value.charAt(0).toUpperCase() + value.substring(1)
   }
 
+  static checkParameters(requiredKeys, receivedObject) {
+    // It must be an object
+    if (typeof receivedObject !== 'object') {
+      throw new Error(`Expected 'object', received '${typeof receivedObject}'`)
+    }
+
+    // Get the keys of the parameter object and sort them
+    const receivedObjectKeys = Object.keys(receivedObject)
+
+    // Maybe we can save CPU cycles (before we crash... )
+    if (receivedObjectKeys.length !== requiredKeys.length) {
+
+      const error = 'Expected an object containing the keys:\n'
+                  + requiredKeys.reduce((p, c) => `${p}, '${c}'`, '').slice(2)
+                  + '\nbut recieved an object containing:\n'
+                  + receivedObjectKeys.reduce((p, c) => `${p}, '${c}'`, '').slice(2)
+
+      throw new Error(error)
+    }
+
+    // We need these sorted, now
+    const recievedObjectKeysSorted = receivedObjectKeys.sort()
+    const requiredKeysSorted = requiredKeys.sort()
+
+    // kickn' it oldskool
+    for (let i = 0; i < requiredKeys.length; i++) {
+
+      // Compare one by one
+      if (recievedObjectKeysSorted[i] !== requiredKeysSorted[i]) {
+        const error = 'Expected an object containing the keys:\n'
+                    + requiredKeys.reduce((p, c) => `${p}, '${c}'`, '').slice(2)
+                    + '\nbut recieved an object containing:\n'
+                    + receivedObjectKeys.reduce((p, c) => `${p}, '${c}'`, '').slice(2)
+        throw new Error(error)
+      }
+
+      // null is not allowed
+      if (receivedObject[requiredKeys[i]] === null ||
+          typeof receivedObject[requiredKeys[i]] === 'undefined') {
+        throw new Error(`${receivedObject[requiredKeys[i]]} cannot be 'null' or 'undefined'`)
+      }
+    }
+
+    // We're good to go
+    return true
+
+  }
 }
 
 export default Util
