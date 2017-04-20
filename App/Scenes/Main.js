7801cf52946a6b842223b83e08769ec772fd5ec2
Werner Roets <cobolt.exe@gmail.com>
2017-04-20 23:01:19 +0200

    added images, fixed null reference

--- App/Scenes/Main.js
+++ App/Scenes/Main.js
@@ -25,7 +25,8 @@ import SearchBox from '../Components/SearchBox'
 import {
   Text,
   View,
-  Dimensions
+  Dimensions,
+  Image
 } from 'react-native'
 import {
   Container,
@@ -46,7 +47,7 @@ export default class Main extends Scene {
       activeTab: TAB_CONNECTED,
       searchText: '',
       connections: [],
-      suggestedConnections: Config.suggestedConnections
+      suggestedConnections: []
     }
     this.fetchingData = false;
   }
@@ -122,7 +123,7 @@ export default class Main extends Scene {
           <LifekeyHeader
             icons={[
               {
-                icon: <Text>+</Text>,
+                icon: <Image source={require("../Images/torn_page.png")}/>,
                 onPress: () => alert('test')
               },
               {
@@ -135,7 +136,7 @@ export default class Main extends Scene {
                 }
               },
               {
-                icon: <Text>+</Text>,
+                icon: <Image source={require("../Images/smiley_speech_bubble.png")}/>,
                 onPress: () => alert('test')
               }
             ]}
