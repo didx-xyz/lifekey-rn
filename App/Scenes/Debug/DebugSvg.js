2820896b8d460db3b61fb1503a02cc6adc1efe0b
Werner Roets <cobolt.exe@gmail.com>
2017-04-26 13:42:39 +0200

    fixed typo

---
+++ App/Scenes/Debug/DebugSvg.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import VerifiedIcon from '../../Components/VerifiedIcon'
+import PeriodIcon from '../../Components/PeriodIcon'
+import MobileIcon from '../../Components/MobileIcon'
+import MarketingIcon from '../../Components/MarketingIcon'
+import LocationIcon from '../../Components/LocationIcon'
+import LandlineIcon from '../../Components/LandlineIcon'
+import InfoIcon from '../../Components/InfoIcon'
+import HexagonIcon from '../../Components/HexagonIcon'
+import HexagonDots from '../../Components/HexagonDots'
+import HelpIcon from '../../Components/HelpIcon'
+import ForwardIcon from '../../Components/ForwardIcon'
+import EnvelopeIcon from '../../Components/EnvelopeIcon'
+import Dots from '../../Components/Dots'
+import BackIcon from '../../Components/BackIcon'
+
+import * as Base from 'native-base'
+
+class DebugSvg extends Component {
+  render() {
+    return (
+      <Base.Container>
+        <Base.Content>
+          <View style={{ flex: 1, alignItems: 'center', padding: 10 }}>
+            <VerifiedIcon width={50} height={50}/>
+            <PeriodIcon width={50} height={50}/>
+            <MobileIcon width={50} height={50}/>
+            <MarketingIcon width={50} height={50}/>
+            <LocationIcon width={50} height={50}/>
+            <LandlineIcon width={50} height={50}/>
+            <InfoIcon width={50} height={50}/>
+            <HexagonIcon width={50} height={50}/>
+            <HexagonDots current={4}/>
+            <HelpIcon width={50} height={50}/>
+            <ForwardIcon width={50} height={50}/>
+            { /*
+            <EnvelopeIcon width={50} height={50}/>
+            <Dots width={50} height={50}/>
+            <BackIcon width={50} height={50}/>
+            */}
+
+          </View>
+        </Base.Content>
+      </Base.Container>
+    )
+  }
+}
+
+export default DebugSvg
