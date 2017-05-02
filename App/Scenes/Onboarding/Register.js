54d4693cb385e1d110b28d1b760147147a4871bf
Werner Roets <cobolt.exe@gmail.com>
2017-05-02 15:31:53 +0200

    hidden status bar

--- App/Scenes/Onboarding/Register.js
+++ App/Scenes/Onboarding/Register.js
@@ -90,23 +90,16 @@ export default class Register extends Scene {
     }
   }
 
-  _onAttention() {
-    StatusBar.setHidden(true)
-  }
-
   componentWillMount() {
     super.componentWillMount()
-    this._onAttention()
   }
 
   componentWillFocus() {
     super.componentWillFocus()
-    this._onAttention()
   }
 
   componentDidFocus() {
     super.componentDidFocus()
-    // this._oti.focus()
   }
 
   componentDidMount() {
@@ -450,6 +443,7 @@ export default class Register extends Scene {
       <Container>
         <Content keyboardShouldPersistTaps="always">
           <AndroidBackButton onPress={() => this._hardwareBackHandler()}/>
+          <StatusBar hidden={true} />
           <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
             <Grid>
               <Col style={[style.sceneColumn, { height: this.props.screenHeight }]}>
