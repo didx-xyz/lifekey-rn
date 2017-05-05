f011a6915371d93be4ed29b362116c0e5d7a130f
Werner Roets <cobolt.exe@gmail.com>
2017-05-05 12:55:48 +0200

    updated things

---
+++ App/Common.js
@@ -0,0 +1,20 @@
+export default class Common {
+    static schemaCheck(url, match){
+        const search = new RegExp(`^(https?\:\/\/)?${url}$`, 'i')
+        const result = search.test(match)
+        return result
+    }
+
+    static schemaHasProtocol(match){
+        const search = new RegExp(`^https?\:\/\/`, 'i')
+        return search.test(match)
+    }
+
+    static ensureUrlHasProtocol(url){
+        if(!Common.schemaHasProtocol(url)){
+          url = `http://${url}`
+        }
+
+        return url
+    }
+}
