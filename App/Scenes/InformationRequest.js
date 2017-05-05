d885c98471344c6551cef582620ce3c328fc05eb
Werner Roets <cobolt.exe@gmail.com>
2017-05-05 14:35:17 +0200

    long items wont render. Wait longer to force update

--- App/Scenes/InformationRequest.js
+++ App/Scenes/InformationRequest.js
@@ -105,7 +105,7 @@ class InformationRequest extends Scene {
     this.updateSwaps()
     setTimeout(() => {
       this.forceUpdate()
-    }, 100)
+    }, 4000)
   }
 
   onPressDecline() {
@@ -349,7 +349,7 @@ class InformationRequest extends Scene {
               Object.values(
                 JSON.parse(resource.value)
               ).filter(item => {
-                return (typeof item === 'string' && item.length > 30) || typeof item !== 'object'
+                return (typeof item === 'string' && item.length < 30) || typeof item !== 'object'
               }).join(', ')
             }
           </Text>
