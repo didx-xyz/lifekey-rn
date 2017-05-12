068d1650e371b91f4a550c52ad77e7ba30d5dae9
Werner Roets <cobolt.exe@gmail.com>
2017-05-12 14:28:35 +0200

    created working version of add

--- App/Models/ConsentThanksMessage.js
+++ App/Models/ConsentThanksMessage.js
@@ -1,9 +1,8 @@
 
 import Logger from '../Logger'
-import {AsyncStorage} from 'react-native'
+import { AsyncStorage } from 'react-native'
 
 const STORAGE_KEY = 'thanks_messages'
-const LOGTAG = 'ThanksMessage'
 
 class ConsentThanksMessage {
 
@@ -33,6 +32,32 @@ class ConsentThanksMessage {
       }
     })
   }
+
+  // static async add(created_at, from_user, amount, reason) {
+  //   const newThanksMessage = { created_at, from_user, amount, reason }
+  //   try {
+  //     const item = await AsyncStorage.getItem(STORAGE_KEY)
+  //     if (item) {
+  //       const newItem = item.concat(newThanksMessage)
+  //       const newItemJSON = JSON.parse(newItem)
+  //       const result = await AsyncStorage.setItem(STORAGE_KEY, newItemJSON)
+  //       if (result) {
+  //         Logger.info('Thanks saved', result)
+  //       } else {
+  //         Logger.warn('Thanks not saved', result)
+  //       }
+  //     }
+  //   } catch (error) {
+  //     Logger.warn('Key does not yet exist', error)
+  //     const firstItemJSON = JSON.parse(newThanksMessage)
+  //     const result = await AsyncStorage.setItem(STORAGE_KEY, firstItemJSON)
+  //     if (result) {
+  //       Logger.info('Thanks saved', result)
+  //     } else {
+  //       Logger.warn('Thanks not saved', result)
+  //     }
+  //   }
+  // }
 
   static all() {
     return AsyncStorage.getItem(
