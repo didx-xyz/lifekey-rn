fa10a91998a2c81b01d0848805b70386b64e96d5
Ant Cosentino <antony@mycosentino.com>
2017-08-02 13:40:08 +0200

    add message

--- App/Components/SceneComponents/AuthScreen.js
+++ App/Components/SceneComponents/AuthScreen.js
@@ -11,7 +11,7 @@ import PropTypes from "prop-types"
 import Touchable from '../../Components/Touchable'
 import ConsentUser from '../../Models/ConsentUser'
 
-import {ActivityIndicator, TextInput, View, Image} from 'react-native'
+import {Text, TextInput, View, Image} from 'react-native'
 import {Container, Content} from 'native-base'
 
 // internal dependencies
@@ -23,37 +23,39 @@ import Dots from '../../Components/Dots'
 class AuthScreen extends Component {
 
   render() {
-   
     return (
-       <Touchable onPress={() => this.pinInput.focus()}>
+      <Touchable onPress={() => this.pinInput.focus()}>
         <View style={style.pinContainer}>
           <View style={Object.assign({}, style.pinElement, {"paddingTop": this.props.paddingTop, "paddingBottom": this.props.paddingBottom})}>
             <HexagonDots height={90}
-                         width={80}
-                         current={this.props.pin.length < 5 ? this.props.pin.length : 4} />
+                        width={80}
+                        current={this.props.pin.length < 5 ? this.props.pin.length : 4} />
           </View>
           <View style={style.pinElement}>
+            {this.props.loading_indicator && (<Text style={style.please_wait}>Please wait...</Text>)}
             <Dots current={this.props.pin.length}
                   fullFill={Palette.consentOffWhite}
                   emptyFill={Palette.consentGrayDarkest}
                   strokeColor="transparent" />
           </View>
-          <ActivityIndicator animating={this.props.loading_indicator} size={'large'} />
           <TextInput ref={(_ref) => this.pinInput = _ref}
-                     autoFocus={true}
-                     returnKeyType="done"
-                     keyboardType="phone-pad"
-                     underlineColorAndroid="transparent"
-                     onChangeText={(text) => this.props.onValueChanged(text)}
-                     style={[style.pinInput]} />
+                    autoFocus={true}
+                    returnKeyType="done"
+                    keyboardType="phone-pad"
+                    underlineColorAndroid="transparent"
+                    onChangeText={(text) => this.props.onValueChanged(text)}
+                    style={[style.pinInput]} />
         </View>
       </Touchable>
     )
-
   }
 }
 
 const style = {
+  please_wait: {
+    textAlign: 'center',
+    color: 'white'
+  },
   "pinInput": {
     height: 0,
     marginLeft: -1000
