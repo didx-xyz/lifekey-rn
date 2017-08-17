4b25f4ba17c6f8cd90e117c550ed940abf8b1e2e
heinschulie <heinschulie@gmail.com>
2017-08-17 15:41:11 +0200

    Fixed index.js mismatch

---
+++ index.android.js
@@ -0,0 +1,11 @@
+/**
+ * Lifekey App
+ * @copyright 2016 Global Consent Ltd
+ * Civvals, 50 Seymour Street, London, England, W1H 7JG
+ * @author Werner Roets <werner@io.co.za>
+ */
+
+import { AppRegistry } from 'react-native'
+import Lifekeyrn from './App/Lifekeyrn'
+
+AppRegistry.registerComponent('Lifekeyrn', () => Lifekeyrn)
--- index.js
+++
@@ -1,5 +0,0 @@
-
-import { AppRegistry } from 'react-native'
-import Lifekeyrn from './App/Lifekeyrn'
-
-AppRegistry.registerComponent('Lifekeyrn', () => Lifekeyrn)
