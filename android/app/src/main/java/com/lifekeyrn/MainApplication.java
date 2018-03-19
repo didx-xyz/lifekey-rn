3a24aab077bb495a7d035a954f9df502d1389541
Zayin Krige <zkrige@gmail.com>
2018-03-19 14:21:14 +0200

    remove crypto code from android - it is now in npm module

--- android/app/src/main/java/com/lifekeyrn/MainApplication.java
+++ android/app/src/main/java/com/lifekeyrn/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.aakashns.reactnativedialogs.ReactNativeDialogsPackage;
 import com.facebook.react.ReactApplication;
+import com.reactlibrary.RNCryptoModulePackage;
 import com.imagepicker.ImagePickerPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
@@ -38,6 +39,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
+            new RNCryptoModulePackage(),
               new ImagePickerPackage(),
               new SvgPackage(),
               new RCTCameraPackage(),
