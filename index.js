2d099f2fc7db2c147230baae11cc93731d82d061
Asatur <asatur@apextechnology.co.za>
2018-05-11 10:04:02 +0400

    Skip isMounted warning

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import { AppRegistry, View } from 'react-native';
+import { AppRegistry, View, YellowBox } from 'react-native';
 import Lifekeyrn from './App/Lifekeyrn';
+
+YellowBox.ignoreWarnings([
+    'Warning: isMounted(...) is deprecated in plain JavaScript React classes.'
+]);
 
 AppRegistry.registerComponent('Lifekeyrn', () => Lifekeyrn);
