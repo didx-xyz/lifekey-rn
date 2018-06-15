1979f6ffea044d7e6f633d64565cd5d08ffa6197
Zayin Krige <zkrige@gmail.com>
2018-06-15 15:05:21 +0200

    remove unused imports

--- App/Common.js
+++ App/Common.js
@@ -1,17 +1,4 @@
 import React from 'react'
-import { Picker, Text, TextInput, View, ScrollView, ToastAndroid } from "react-native"
-import { Container } from "native-base"
-import Touchable from "./Components/Touchable"
-import ModalPicker from "react-native-modal-selector"
-import DatePicker from "react-native-datepicker"
-import PropTypes from "prop-types"
-import ImagePicker from "react-native-image-picker"
-import ActivityIndicator from "ActivityIndicator"
-
-import Countries from "./Countries"
-import Languages from "./Languages"
-
-import BackButton from "./Components/BackButton"
 
 export default class Common {
     static schemaCheck(url, match){
