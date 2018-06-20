2ec70c82d582abac55d8a72bfa0df83546740c23
Zayin Krige <zkrige@gmail.com>
2018-06-20 14:29:16 +0200

    Revert "Description : open keyboard on focus"
    This reverts commit 3fd42fe0e69b0e0d90cb1bad84a120be1f755490.

--- App/Components/SceneComponents/AuthScreen.js
+++ App/Components/SceneComponents/AuthScreen.js
@@ -6,13 +6,13 @@
  * @author Hein <hein@io.co.za>
  */
 
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PropTypes from "prop-types"
 import Touchable from '../../Components/Touchable'
 import ConsentUser from '../../Models/ConsentUser'
 
-import { Text, TextInput, View, Image, Platform } from 'react-native'
-import { Container, Content } from 'native-base'
+import {Text, TextInput, View, Image} from 'react-native'
+import {Container, Content} from 'native-base'
 
 // internal dependencies
 import Design from "../../DesignParameters"
@@ -21,44 +21,30 @@ import HexagonDots from '../../Components/HexagonDots'
 import Dots from '../../Components/Dots'
 
 class AuthScreen extends Component {
-  state = {
-    openKeyBoard: false
-  }
 
   render() {
     return (
-      <Touchable onPress={() => {
-        if (Platform.OS === "ios") {
-          this.pinInput.focus();
-        } else {
-          this.pinInput.blur();
-        }
-      }}>
+      <Touchable onPress={() => this.pinInput.focus()}>
         <View style={style.pinContainer}>
-          <View style={Object.assign({}, style.pinElement, { "paddingTop": this.props.paddingTop, "paddingBottom": this.props.paddingBottom })}>
+          <View style={Object.assign({}, style.pinElement, {"paddingTop": this.props.paddingTop, "paddingBottom": this.props.paddingBottom})}>
             <HexagonDots height={90}
-              width={80}
-              current={this.props.pin.length < 5 ? this.props.pin.length : 4} />
+                        width={80}
+                        current={this.props.pin.length < 5 ? this.props.pin.length : 4} />
           </View>
           <View style={style.pinElement}>
             {this.props.loading_indicator && (<Text style={style.please_wait}>Please wait...</Text>)}
             <Dots current={this.props.pin.length}
-              fullFill={Palette.consentOffWhite}
-              emptyFill={Palette.consentGrayDarkest}
-              strokeColor="transparent" />
+                  fullFill={Palette.consentOffWhite}
+                  emptyFill={Palette.consentGrayDarkest}
+                  strokeColor="transparent" />
           </View>
           <TextInput ref={(_ref) => this.pinInput = _ref}
-            autoFocus={true}
-            returnKeyType="done"
-            keyboardType="phone-pad"
-            onBlur={() => {
-              if (Platform.OS === "android") {
-                this.pinInput.focus();
-              }
-            }}
-            underlineColorAndroid="transparent"
-            onChangeText={(text) => this.props.onValueChanged(text)}
-            style={[style.pinInput]} />
+                    autoFocus={true}
+                    returnKeyType="done"
+                    keyboardType="phone-pad"
+                    underlineColorAndroid="transparent"
+                    onChangeText={(text) => this.props.onValueChanged(text)}
+                    style={[style.pinInput]} />
         </View>
       </Touchable>
     )
@@ -74,13 +60,13 @@ const style = {
     height: 0,
     marginLeft: -1000
   },
-  "pinContainer": {
+  "pinContainer": { 
     "width": "100%",
     "minHeight": 200
   },
   "pinElement": {
     "flex": 1,
-    "justifyContent": "center",
+    "justifyContent": "center", 
     "alignItems": "center"
   }
 }
--- android/app/src/main/java/com/lifekeyrn/MainApplication.java
+++ android/app/src/main/java/com/lifekeyrn/MainApplication.java
@@ -4,7 +4,6 @@ import android.support.multidex.MultiDexApplication;
 
 import com.aakashns.reactnativedialogs.ReactNativeDialogsPackage;
 import com.facebook.react.ReactApplication;
-import za.co.apextechnology.crypto.CryptoPackage;
 import com.microsoft.codepush.react.CodePush;
 import za.co.apextechnology.crypto.CryptoPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -51,6 +50,7 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
                     new SvgPackage(),
                     new RNCameraPackage(),
                     new VectorIconsPackage(),
+                    new CryptoPackage(),
                     new ReactNativeDialogsPackage(),
                     new FingerprintPackage(),
                     new RNFirebasePackage(),
